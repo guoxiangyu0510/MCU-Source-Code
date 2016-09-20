24c39d1676f6fce85749063fdef54263031db667
guoxiangyu0510 <903367226@qq.com>
2016-09-20 22:17:39 +0800

    123123
    1111111

--- AutoBox/include/eeprom.h
+++
@@ -1,8 +0,0 @@
-#ifndef _EEPROM_
-#define _EEPROM_
-	typedef unsigned char BYTE;
-	typedef unsigned int WORD;
-	extern BYTE IapReadByte(WORD addr);
-	extern void IapProgramByte(WORD addr, BYTE dat);
-	extern void IapEraseSector(WORD addr);
-#endif
